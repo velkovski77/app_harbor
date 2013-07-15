05e5cd907fcbd6d476c9ef5e04af04cfe191bb6e
dragan777 <draganvelkovski77@gmail.com>
2013-07-15 22:45:22 +0200

    Adddng jQuery slider

---
+++ app_harbor/Scripts/Slider.js
@@ -0,0 +1,36 @@
+﻿function slideSwitch() {
+    var $active = $('#slideshow IMG.active');
+    if ($active.length == 0) $active = $('#slideshow IMG:last');
+    var $next = $active.next().length ? $active.next() : $('#slideshow IMG:first');
+    $active.addClass('last-active');
+
+    $next.css({ opacity: 0.0 })
+        .addClass('active')
+        .animate({ opacity: 1.0 }, 1000, function () {
+            $active.removeClass('active last-active');
+        });
+}
+function previousSwitch() {
+    var $active = $('#slideshow IMG.active');
+    if ($active.length == 0) $active = $('#slideshow IMG:last');
+    var $prev = $active.prev().length ? $active.prev() : $('#slideshow IMG:last');
+    $active.addClass('last-active');
+
+    $prev.css({ opacity: 0.0 })
+    .addClass('active')
+    .animate({ opacity: 1.0 }, 1000, function () {
+        $active.removeClass('active last-active');
+    });
+}
+
+$(function () {
+    previousSwitch();
+    setInterval("slideSwitch()", 7000);
+    $("#prev").click(function () {
+        previousSwitch();
+    });
+    $("#next").click(function () {
+        slideSwitch();
+
+    });
+});
---
+++ app_harbor/Scripts/myScripts.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $("#main").fadeIn(4000);
+
+    $('#showslider').click(function () {
+        
+        $("#sliderhide").toggle('slow', function () {
+            // Animation complete.
+        });
+    });
+});
